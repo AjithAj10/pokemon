4916fa087c9221e0a558f0f989cc9fcf3264e086
Aji <ajith.r@kraftshala.com>
2023-06-30 15:32:53 +0530

    bokkmark style

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -9,7 +9,7 @@ function Sidebar() {
 
   return (
     <div className='sidebar'>
-      <Link to={'/bookmarks'} >bookmarks</Link>
+      <Link to={'/bookmarks'} style={{marginRight:'2em',maxHeight:'30px',padding:'1em'}} >bookmarks</Link>
             filters:
             <div className="typeFilter">
               {allData?.length > 0 && <MenuBar title={'type'} data={allData} /> }
